1e10e1be551c1db79b59ff3b88c37614e2e2b8bf
andrelay13 <55075194+andrelay13@users.noreply.github.com>
2019-09-24 17:04:00 +0700

    Update main.c

--- helloworld/helloworld/main.c
+++ helloworld/helloworld/main.c
@@ -2,22 +2,9 @@
 
 int main()
 {
-//    int M,N,;
-//
-//    printf("Input angka awal: ");
-//    scanf("%d",&M);getchar();
-//
-//    printf("\nInput jumlah angka yang diinginkan: ");
-//    scanf("%d",&N);
-//
-//    for(i=M;i<=N;i++)
-//    {
-//        printf("%d\n",i);
-//    }
-
    
-
-    return 0;
-           
-
+   printf("Hello World!\n)
+   return 0;
+ 
+         
 }
