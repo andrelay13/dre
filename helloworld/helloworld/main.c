d9c0d99f7e5aa8ff4831d963e9434a76eac6af1d
andrelay13 <55075194+andrelay13@users.noreply.github.com>
2019-09-24 17:01:40 +0700

    Add files via upload
    Newbie in coding

---
+++ helloworld/helloworld/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+//    int M,N,;
+//
+//    printf("Input angka awal: ");
+//    scanf("%d",&M);getchar();
+//
+//    printf("\nInput jumlah angka yang diinginkan: ");
+//    scanf("%d",&N);
+//
+//    for(i=M;i<=N;i++)
+//    {
+//        printf("%d\n",i);
+//    }
+
+   
+
+    return 0;
+           
+
+}
